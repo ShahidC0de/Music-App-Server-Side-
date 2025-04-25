fd8ae428a97ef43b1efd96c0854c383c6b5efebf
ShahidC0de <shahidzada.cs@gmail.com>
2025-04-26 00:33:48 +0500

    signup and login functionalities are done with proper decent format and structure

--- routes/auth.py
+++ routes/auth.py
@@ -5,14 +5,16 @@ from fastapi import Depends, HTTPException
 from database import get_db
 from models.user import User
 from fastapi import APIRouter
-from pydantic_schemas.create_user import CreateUser
 from sqlalchemy.orm import Session
+
+from pydantic_schemas.user_create import CreateUser
+from pydantic_schemas.user_login import UserLogin
 router = APIRouter()
 
 
-@router.post('/signup')
+@router.post('/signup',status_code= 201)
 def user_signup(user: CreateUser, db: Session = Depends(get_db)):
-    db.query(User).all()
+    # db.query(User).all()
      # it will return all the users present in the user table;
     user_db = db.query(User).filter(User.email == user.email).first() # all, search for all or return the first one.
     # the first User.email = db, user.email = requested through json;
@@ -25,3 +27,15 @@ def user_signup(user: CreateUser, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(user_db)
     return user_db
+@router.post('/login')
+def user_login(user: UserLogin, db: Session = Depends(get_db)):
+   user_db =  db.query(User).filter(User.email == user.email).first()
+   if not user_db:
+       raise HTTPException(400, "User with this email does not exist")
+   
+   is_matched = bcrypt.checkpw(user.password.encode(), user_db.password)
+   if not is_matched:
+       raise HTTPException(400, "Password not matched")
+   return user_db
+   
+       
